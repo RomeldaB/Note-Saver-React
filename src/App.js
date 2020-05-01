e1ed44f3dfaed10dbfc8d6ae63d59c478fa1febd
Romelda <bromelda@yahoo.com>
2020-05-01 20:24:15 +0200

    Create project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Authentication from "./pages/Authentication";
 import "./App.css";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/register" exact>
+          <Authentication page="register" />
+        </Route>
+        <Route path="/login" exact>
+          <Authentication page="login" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/Authentication.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Authentication({ page }) {
+  return <div>Authentication {page}</div>;
+}
