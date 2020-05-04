9706df181d814e67152b686d78745c631cd315a7
Romelda <bromelda@yahoo.com>
2020-05-04 12:17:18 +0200

    Add build folder

---
+++ precache-manifest.7797eb4e5234a58596c9ec7ba8468901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea64907a5b3faab925a4f2d84d342c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa68eda9abef8192d7f6",
+    "url": "/static/css/main.f5959f7d.chunk.css"
+  },
+  {
+    "revision": "ed9ee194bf89632f6b9a",
+    "url": "/static/js/2.515d771a.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/static/js/2.515d771a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa68eda9abef8192d7f6",
+    "url": "/static/js/main.2282ffe4.chunk.js"
+  },
+  {
+    "revision": "420e90d25d756f6d8af3",
+    "url": "/static/js/runtime-main.e56cac76.js"
+  }
+]);
